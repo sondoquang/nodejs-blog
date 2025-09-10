b0109a2a966e9e985d46f10b835357ca23af054b
Son(STLang) <sondoquang3@gmail.com>
2025-09-10 21:26:19 +0700

    Config static file

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+const {engine} = require('express-handlebars')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// http logger
+app.use(morgan('combined'))
+
+// template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+// routes
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ src/resources/views/partials/footer.hbs
@@ -0,0 +1,3 @@
+<footer>
+    <h1>HEADER</h1>
+</footer>
